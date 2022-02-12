23cdbcdbf9797ce048d8eb79c38e5c0fc2a2b161
Thomas Lindae <thomas.lindae@in.tum.de>
2022-02-12 07:39:01 +0100

    implemented commutative_binop and forward_ref_commutative_binop

--- src/lib.rs
+++ src/lib.rs
@@ -502,6 +502,127 @@ macro_rules! forward_ref_binop {
     };
 }
 
+/// For types `T: Copy`, `U: Copy` for which binary operator `binop` is implemented (`T binop U`), also implement `U binop T`.
+/// This macro will fail if `LHS` = `RHS`.
+///
+/// For readability, the expected syntax of the macro is the following:
+/// ```text
+/// ( [ Generics ] )?
+/// impl Trait, Method for LHS(, RHS)?
+/// ( where Bounds )?
+/// ```
+/// - `Generics` are comma-seperated type or const generics
+/// - `Trait` is the trait to be implemented
+/// - `Method` is the method that `Trait` defines\
+///   (can be ommitted for [`Add`](https://doc.rust-lang.org/std/ops/trait.Add.html) and [`Mul`](https://doc.rust-lang.org/std/ops/trait.Mul.html))
+/// - `LHS` is the type of the left hand side of the original operation (i.e. `T`)
+/// - `RHS` is the type of the right hand side of the original operation (i.e. `U`)
+/// - `Bounds` are comma-seperated trait bounds for the listed generics
+///
+/// Note in particular that `LHS` and `RHS` denote the left and right side of the **original** operation, not the one being created. The reason for this is to be consistent with all other macros in this crate, even if it seems unintuitive.
+#[macro_export]
+macro_rules! commutative_binop {
+    (
+        $( [ $($generic:tt)* ] )?
+        impl Add for $lhs:ty, $rhs:ty
+        $( where $($bound:tt)* )?
+    ) => {
+        commutative_binop! {
+            $( [ $($generic)* ] )?
+            impl Add, add for $lhs, $rhs
+            $( where $($bound)* )?
+        }
+    };
+    (
+        $( [ $($generic:tt)* ] )?
+        impl Mul for $lhs:ty, $rhs:ty
+        $( where $($bound:tt)* )?
+    ) => {
+        commutative_binop! {
+            $( [ $($generic)* ] )?
+            impl Mul, mul for $lhs, $rhs
+            $( where $($bound)* )?
+        }
+    };
+
+    (
+        $( [ $($generic:tt)* ] )?
+        impl $impl:ident, $meth:ident for $lhs:ty, $rhs:ty
+        $( where $($bound:tt)* )?
+    ) => {
+        impl$(<$($generic)*>)? $impl<$lhs> for $rhs
+        $(where
+            $($bound)*)?
+        {
+            type Output = <$lhs as $impl<$rhs>>::Output;
+
+            fn $meth(self, rhs: $lhs) -> Self::Output {
+                <$lhs>::$meth(rhs, self)
+            }
+        }
+    };
+}
+
+/// For types `T: Copy`, `U: Copy` for which binary operator `binop` is implemented commutatively (`T binop U` **and** `U binop T`), also implement `T binop &U`, `&T binop U`, `&T binop &U`, `U binop &T`, `&U binop T` and `&U binop &T`.
+/// This macro will fail if `LHS` = `RHS`.
+///
+/// For readability, the expected syntax of the macro is the following:
+/// ```text
+/// ( [ Generics ] )?
+/// impl Trait, Method for LHS(, RHS)?
+/// ( where Bounds )?
+/// ```
+/// - `Generics` are comma-seperated type or const generics
+/// - `Trait` is the trait to be implemented
+/// - `Method` is the method that `Trait` defines\
+///   (can be ommitted for [`Add`](https://doc.rust-lang.org/std/ops/trait.Add.html) and [`Mul`](https://doc.rust-lang.org/std/ops/trait.Mul.html))
+/// - `LHS` is the type of the left hand side of the original operation (i.e. `T`)
+/// - `RHS` is the type of the right hand side of the original operation (i.e. `U`)
+/// - `Bounds` are comma-seperated trait bounds for the listed generics
+#[macro_export]
+macro_rules! forward_ref_commutative_binop {
+    (
+        $( [ $($generic:tt)* ] )?
+        impl Add for $lhs:ty, $rhs:ty
+        $( where $($bound:tt)* )?
+    ) => {
+        forward_ref_commutative_binop! {
+            $( [ $($generic)* ] )?
+            impl Add, add for $lhs, $rhs
+            $( where $($bound)* )?
+        }
+    };
+    (
+        $( [ $($generic:tt)* ] )?
+        impl Mul for $lhs:ty, $rhs:ty
+        $( where $($bound:tt)* )?
+    ) => {
+        forward_ref_commutative_binop! {
+            $( [ $($generic)* ] )?
+            impl Mul, mul for $lhs, $rhs
+            $( where $($bound)* )?
+        }
+    };
+
+    (
+        $( [ $($generic:tt)* ] )?
+        impl $impl:ident, $meth:ident for $lhs:ty, $rhs:ty
+        $( where $($bound:tt)* )?
+    ) => {
+        crate::forward_ref_binop! {
+            $( [ $($generic)* ] )?
+            impl $impl, $meth for $lhs, $rhs
+            $( where $($bound)* )?
+        }
+
+        crate::forward_ref_binop! {
+            $( [ $($generic)* ] )?
+            impl $impl, $meth for $rhs, $lhs
+            $( where $($bound)* )?
+        }
+    };
+}
+
 /// For types `T: Copy`, `U: Copy` for which assignment operator `assop` is implemented (`T assop U`), also implement `T assop &U`.
 ///
 /// For readability, the expected syntax of the macro is the following:
--- tests/binary.rs
+++ tests/binary.rs
@@ -1,4 +1,4 @@
-use forward_ref_generic::forward_ref_binop;
+use forward_ref_generic::{commutative_binop, forward_ref_binop, forward_ref_commutative_binop};
 use std::ops::Add;
 
 mod no_generic {
@@ -114,5 +114,57 @@ mod complicated_generics {
         assert_eq!(p1 + p2, p1 + &p2);
         assert_eq!(p1 + p2, &p1 + p2);
         assert_eq!(p1 + p2, &p1 + &p2);
+    }
+}
+
+mod commutative {
+    use super::{commutative_binop, forward_ref_commutative_binop, Add};
+
+    #[derive(Clone, Copy, PartialEq)]
+    struct Int1(i32);
+
+    #[derive(Clone, Copy, PartialEq)]
+    struct Int2(i32);
+
+    impl Add<Int2> for Int1 {
+        type Output = i32;
+
+        fn add(self, rhs: Int2) -> Self::Output {
+            self.0 + rhs.0
+        }
+    }
+
+    commutative_binop! {
+        impl Add for Int1, Int2
+    }
+
+    forward_ref_commutative_binop! {
+        impl Add for Int1, Int2
+    }
+
+    #[test]
+    fn add_commutative_only() {
+        let int1 = Int1(5);
+        let int2 = Int2(3);
+
+        assert_eq!(int1 + int2, 5 + 3);
+        assert_eq!(int2 + int1, 3 + 5);
+    }
+
+    #[test]
+    fn add_forward_ref_commutative() {
+        let int1 = Int1(5);
+        let int2 = Int2(3);
+
+        assert_eq!(int1 + int2, 5 + 3);
+        assert_eq!(int2 + int1, 3 + 5);
+
+        assert_eq!(&int1 + int2, 5 + 3);
+        assert_eq!(int1 + &int2, 5 + 3);
+        assert_eq!(&int1 + &int2, 5 + 3);
+
+        assert_eq!(&int2 + int1, 3 + 5);
+        assert_eq!(int2 + &int1, 3 + 5);
+        assert_eq!(&int2 + &int1, 3 + 5);
     }
 }
